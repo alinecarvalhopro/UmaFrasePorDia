6860285846e87e81ebf9d763f35d150b9a82e8f8
Aline Carvalho <alinecarvalho.pro@gmail.com>
2023-08-07 12:56:33 -0300

    feat: initial settings

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>Uma frase por dia</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
